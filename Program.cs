06caa16b0da8fcd90e056eabc201713e29b7d8f7
Aunt D <dzavizistur@gmail.com>
2025-04-03 11:08:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace N4__YP__Task_1_03._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            double sum = 0; 
+            int count = 0;  
+            int.TryParse(Console.ReadLine(), out int A);
+
+            while (true)
+            {
+                count++;                          // Увеличиваем счетчик слагаемых
+                double term = Math.Pow(count, count); // Вычисляем текущее слагаемое n^n
+                sum += term;                      // Добавляем его к сумме
+
+                if (sum > A)                     
+                {break;}
+            }
+                Console.WriteLine($"Количество слагаемых: {count}");
+        }
+    }
+}
