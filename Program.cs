0902bf3fdab64e0b99a267b896fd0330fc7195b5
Aunt D <dzavizistur@gmail.com>
2025-04-04 09:00:55 +0300

    Добавьте файлы проекта.del

--- Program.cs
+++ Program.cs
@@ -5,16 +5,20 @@
         static void Main()
         {
 
-            int j = 2;
-            for (int i = 2; i < 32; i = i * 2)
-            {
-                while (i < j)
-                {
-                    j = j * 2;
-                }
-                i = j - i;
-            }
+            double sum = 0;
+            int count = 0;
+            int.TryParse(Console.ReadLine(), out int A);
 
+            while (true)
+            {
+                count++;                          // Увеличиваем счетчик слагаемых
+                double term = Math.Pow(count, count); // Вычисляем текущее слагаемое n^n
+                sum += term;                      // Добавляем его к сумме
+
+                if (sum > A)
+                { break; }
+            }
+            Console.WriteLine($"Количество слагаемых: {count}");
         }
     }
 }
